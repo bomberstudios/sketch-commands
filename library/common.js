39ebd11dadbfbcbf8d8b8d85838b57b00b6e4b79
Ale Muñoz <bomberstudios@gmail.com>
2012-11-29 00:59:11 +0100

    Renamed command

--- library/common.js
+++ library/common.js
@@ -18,25 +18,47 @@ com.bomberstudios = {
     var file_manager = [NSFileManager defaultManager];
     [file_manager createDirectoryAtPath:path withIntermediateDirectories:true attributes:nil error:nil];
   },
-  export_all_slices: function(format){
+  export_all_slices: function(format,path){
+    if (path == undefined) {
+      path = export_folder;
+    }
     pages.each(function(page){
       [doc setCurrentPage:page];
 
       var layers = [[doc currentPage] allSlices];
       layers.each(function(slice){
-        [doc saveArtboardOrSlice:slice toFile:export_folder + [slice name] + "." + format];
+        [doc saveArtboardOrSlice:slice toFile:path + [slice name] + "." + format];
       });
     });
   },
-  export_all_artboards: function(format){
+  export_all_artboards: function(format,path){
+    if (path == undefined) {
+      path = export_folder;
+    }
     pages.each(function(page){
       [doc setCurrentPage:page];
 
       var layers = [[doc currentPage] artboards];
       layers.each(function(artboard){
-        [doc saveArtboardOrSlice:artboard toFile:export_folder + [artboard name] + "." + format];
+        [doc saveArtboardOrSlice:artboard toFile:path + [artboard name] + "." + format];
       });
     });
+  },
+  padNumber: function(num){
+    num = num.toString();
+    if (num.length < 2) {
+      num = "0" + num;
+    };
+    return num;
+  },
+  isodate: function(){
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = com.bomberstudios.padNumber((now.getMonth() + 1));
+    var day = com.bomberstudios.padNumber(now.getDate());
+    var hour = com.bomberstudios.padNumber(now.getHours());
+    var minute = com.bomberstudios.padNumber(now.getMinutes());
+    return year + month + day + hour + minute;
   }
 };
 
