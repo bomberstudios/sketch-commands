3d4b5438d2cb509490797a7cce8248e481ad504d
Ale Muñoz <bomberstudios@gmail.com>
2014-01-25 16:42:33 +0100

    Clarify message in sandbox panel

--- library/sandbox-sketch-utils.js
+++ library/sandbox-sketch-utils.js
@@ -4,7 +4,7 @@ function in_sandbox(){
   return (nil != [environ objectForKey:@"APP_SANDBOX_CONTAINER_ID"]);
 }
 var sandboxAccess = AppSandboxFileAccess.init({
-  message: "Please authorize Sketch to write to this folder. You will only need to do this once.",
+  message: "Please authorize Sketch to write to this folder. You will only need to do this once per folder.",
   prompt:  "Authorize",
   title: "Sketch Authorization"
 })
