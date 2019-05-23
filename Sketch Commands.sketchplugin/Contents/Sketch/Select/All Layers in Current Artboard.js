667e1fdec536761fc7979d3eade29c0344dca157
Ale M <bomberstudios@gmail.com>
2019-05-23 13:53:46 +0200

    Port Select/All Layers in Current Artboard to JS API

---
+++ Sketch Commands.sketchplugin/Contents/Sketch/Select/All Layers in Current Artboard.js
@@ -0,0 +1,12 @@
+var onRun = function(context) {
+  var sketch = require('sketch')
+  var Document = sketch.Document
+  var document = Document.getSelectedDocument()
+
+  var selection = document.selectedLayers
+  selection.clear()
+  
+  var page = document.selectedPage
+  var currentArtboard = sketch.Artboard.fromNative(page._object.currentArtboard())
+  currentArtboard.layers.forEach(layer => layer.selected = true)
+}
