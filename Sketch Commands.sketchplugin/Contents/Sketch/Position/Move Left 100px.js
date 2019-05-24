3fe530b09b92450018863e18f30283b8458b70e3
Ale M <bomberstudios@gmail.com>
2019-05-24 21:19:28 +0200

    Ported Move Commands to JS API

---
+++ Sketch Commands.sketchplugin/Contents/Sketch/Position/Move Left 100px.js
@@ -0,0 +1,10 @@
+//Move Left 100px
+
+var onRun = function(context) {
+  var sketch = require('sketch')
+  var document = sketch.Document.getSelectedDocument()
+  var selection = document.selectedLayers
+  selection.forEach(layer => {
+    layer.frame.x -= 100
+  })
+}
---
+++ Sketch Commands.sketchplugin/Contents/Sketch/Position/Move Up 100px.js
@@ -0,0 +1,10 @@
+//Move Up 100px
+
+var onRun = function(context) {
+  var sketch = require('sketch')
+  var document = sketch.Document.getSelectedDocument()
+  var selection = document.selectedLayers
+  selection.forEach(layer => {
+    layer.frame.y -= 100
+  })
+}
